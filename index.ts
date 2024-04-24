dfb80b5724b0ac1eef73fd6377429f0e7028cbfa
ian-chu-job-pair <162425644+en-job-pair@users.noreply.github.com>
2024-04-24 16:56:12 +0800

    add plugin

--- index.ts
+++ index.ts
@@ -1,15 +1,45 @@
-import fastify from 'fastify'
-
-const server = fastify()
-
-server.get('/ping', async (request, reply) => {
-    return 'pong\n'
-})
-
-server.listen({ port: 8080 }, (err, address) => {
-    if (err) {
-        console.error(err)
-        process.exit(1)
-    }
-    console.log(`Server listening at ${address}`)
-})
+'use strict'
+console.time('Server boot')
+// Plugins
+import path from 'path'
+import AutoLoad from '@fastify/autoload'
+import FormBody from '@fastify/formbody'
+// Models 
+import Industry from './models/industry.js'
+import Location from './models/location.js'
+import Question from './models/question.js'
+import Select from './models/select.js'
+// initilize server
+export default async function (fastify, opts) {
+  const { ready, } = fastify
+  const __dirname = path.resolve()
+  // Plugins
+  fastify.register(Cache, {
+    includeRoutes: ['/select']
+  })
+  fastify.register(FormBody)
+  fastify.register(UUID)
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'plugins'),
+    options: Object.assign({}, opts),
+    ignorePattern: /.*(uuid|socketio|cache).js/
+  })
+  // Models
+  fastify.register(Industry)
+  fastify.register(Location)
+  fastify.register(Question)
+  fastify.register(Select)
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'models'),
+    options: Object.assign({}, opts),
+    ignorePattern: /.*(location|question|select|industry).js/
+  })
+  // Conterollers
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'controllers'),
+    options: Object.assign({}, opts)
+  })
+  ready(() => {
+    console.timeEnd('Server boot')
+  })
+}
