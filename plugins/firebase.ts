a8944a1624be191f5468b063b167d5575843e3d5
ian-chu-job-pair <162425644+en-job-pair@users.noreply.github.com>
2024-04-25 17:15:20 +0800

    local error unable to solve

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -1,5 +1,9 @@
 import fp from 'fastify-plugin'
-const { initializeApp, applicationDefault } = require('firebase-admin/app');
+import fs from 'fs'
+import path from 'path'
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+// https://github.com/firebase/firebase-functions/issues/1425
+const admin = require("firebase-admin");
 const { getFirestore, } = require('firebase-admin/firestore');
 export class Firebase {
     firestore: any
@@ -7,9 +11,27 @@ export class Firebase {
         /**
          * https://firebase.google.com/docs/reference/admin/node/firebase-admin.app.md#applicationdefault_2121df4
          */
-        initializeApp({
-            credential: applicationDefault(),
-        })
+        const serviceAccount = path.join(__dirname, '../serviceAccountKey.json')
+        // console.log(pathToKey);
+        // var serviceAccount = require("path/to/serviceAccountKey.json");
+
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount)
+        });
+        // const credential = cert(pathToKey)
+        // console.log(credential);
+        // try {
+        //     initializeApp({
+        //         credential,
+        //         // databaseURL: "https://<DATABASE_NAME>.firebaseio.com"
+        //     });
+        // } catch (error) {
+        //     console.log(error)
+        // }
+        // console.log('?');
+        // initializeApp({
+        //     credential: applicationDefault(),
+        // })
         this.firestore = getFirestore();
     }
 }
