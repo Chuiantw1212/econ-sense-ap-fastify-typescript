33c0ad25ed6c7eb92b620d7476ae840b3f721f8d
ian-chu-job-pair <162425644+en-job-pair@users.noreply.github.com>
2024-04-25 14:16:00 +0800

    add select

--- models/select.ts
+++ models/select.ts
@@ -1,30 +1,66 @@
 import axios from 'axios'
 import fp from 'fastify-plugin'
-import type { extendsFastifyInstance } from '../types/fastify.js'
-const { XMLParser, XMLBuilder, XMLValidator } = require("fast-xml-parser");
+const { XMLParser, } = require("fast-xml-parser");
+
+interface ISelectItem {
+    name: string,
+    code: string,
+    code01: number,
+}
 
 interface ICounty {
-    code: string,
-    name: string,
+    countycode: string,
+    countyname: string,
     countycode01: number,
 }
 
+interface ITown {
+    towncode: string,
+    townname: string,
+    towncode01: number,
+}
+
+interface ITownMap {
+    [key: string]: ISelectItem[];
+}
+
 export class Select {
-    counties: ICounty[] = []
-    constructor(fastify: extendsFastifyInstance) {
-        if (!this.counties.length) {
-            this.setCounties()
-        }
+    counties: ISelectItem[] = []
+    towns: ITownMap = {}
+    constructor() {
+        this.setCountiesAndTowns()
     }
-    async setCounties() {
+    async setCountiesAndTowns() {
+        // Set counties from https://data.gov.tw/dataset/101905
         const result = await axios.get('https://api.nlsc.gov.tw/other/ListCounty')
         const parser = new XMLParser();
         const jsonResult = parser.parse(result.data);
         const countyItems = jsonResult.countyItems.countyItem
-        this.counties = countyItems
+        this.counties = countyItems.map((item: ICounty) => {
+            return {
+                code: item.countycode,
+                name: item.countyname,
+                code01: item.countycode01,
+            }
+        })
+        // Set towns from https://data.gov.tw/dataset/102011
+        const promises = this.counties.map((county: ISelectItem) => {
+            const promise = axios.get(`https://api.nlsc.gov.tw/other/ListTown1/${county.code}`)
+            return promise
+        })
+        const townResults = await Promise.all(promises)
+        this.counties.forEach((county: ISelectItem, index) => {
+            this.towns[county.code] = townResults[index].data.map((item: ITown) => {
+                return {
+                    name: item.townname,
+                    code: item.towncode,
+                    code01: item.towncode01,
+                }
+            })
+        })
     }
 }
 export default fp(async function (fastify: any, opts) {
-    const selectModel = new Select(fastify)
+    const selectModel = new Select()
     fastify.decorate('SelectModel', selectModel)
 })
