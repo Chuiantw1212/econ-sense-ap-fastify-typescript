67c6aa433482ecbe7265ab4a34c0e017b3178cb2
Chuian <chuiantw1212@gmail.com>
2024-05-05 08:44:46 +0800

    split config

--- controllers/bank.ts
+++ controllers/bank.ts
@@ -5,15 +5,18 @@ export default fp(async function (fastify) {
     const {
         BankModel
     } = fastify as extendsFastifyInstance
-    fastify.get('/bank/config', async function (req: FastifyRequest, res: FastifyReply) {
+    fastify.get('/bank/config/interestRate', async function (req: FastifyRequest, res: FastifyReply) {
         try {
             const interestRate = await BankModel.fetchInterestRate()
+            res.code(200).send(interestRate)
+        } catch (error: any) {
+            res.code(500).send(error.message || error)
+        }
+    })
+    fastify.get('/bank/config/portfolioIrr', async function (req: FastifyRequest, res: FastifyReply) {
+        try {
             const portfolioIRR = await BankModel.fetchCoreSeriesIRR()
-            const response = {
-                interestRate,
-                portfolioIRR
-            }
-            res.code(200).send(response)
+            res.code(200).send(portfolioIRR)
         } catch (error: any) {
             res.code(500).send(error.message || error)
         }
