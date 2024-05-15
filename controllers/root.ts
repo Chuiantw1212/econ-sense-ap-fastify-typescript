d43c539b814b0de9dec8b5a050c481b0e40a39a0
Chuian <chuiantw1212@gmail.com>
2024-05-15 11:49:58 +0800

    add memory usage

--- controllers/root.ts
+++ controllers/root.ts
@@ -3,8 +3,24 @@ import fp from 'fastify-plugin'
 import { memoryUsage } from 'node:process';
 export default fp(async function (fastify) {
     fastify.get('/', async function (req: FastifyRequest, res: FastifyReply) {
+        const memoryUsageInMB: ReturnType<typeof memoryUsage> = {
+            rss: 0,
+            heapTotal: 0,
+            heapUsed: 0,
+            arrayBuffers: 0,
+            external: 0,
+        }
+        const currentMemoryUsage: any = memoryUsage()
+        for (let key in memoryUsageInMB) {
+            const mb: number = Math.floor(1024 * 1024)
+            const value: number = currentMemoryUsage[key]
+            const valueInMB: number = Math.floor(value / mb)
+            Object.assign(memoryUsageInMB, { 
+                [key]: `${valueInMB.toLocaleString()}Mb`
+            })
+        }
         res.code(200).send({
-            memoryUsage: memoryUsage()
+            memoryUsage: memoryUsageInMB
         })
     })
 })
